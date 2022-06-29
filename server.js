130d429e8e20e28851c0c244fec299d7c2b7d0e5
kierstenv <kierstenvicknair22@gmail.com>
2022-06-29 05:13:56 -0500

    add server and heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
